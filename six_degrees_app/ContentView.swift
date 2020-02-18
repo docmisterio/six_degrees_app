47cba8f29fb90c83fb1fd288d7aa7c644306d62c
Zach Becker <docmisterio@Trevor-the-MacBook-Pro.local>
2020-02-17 20:01:24 -0600

    initial commit - initial project creation in Xcode

---
+++ six_degrees_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  six_degrees_app
+//
+//  Created by Zach Becker on 2/12/20.
+//  Copyright © 2020 Zach Becker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
