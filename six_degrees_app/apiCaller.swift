3823d5ce66dda30c2385243626b417f65e3f5bed
Zach Becker <doc.misterio@gmail.com>
2020-02-26 20:42:46 -0600

    added what I needed to add including the init to the struct. I think I need to move this to a UIVC class in the viewDidLoad func... but I'm not super sure why it seems like the init isn't needed if done there.

--- six_degrees_app/apiCaller.swift
+++ six_degrees_app/apiCaller.swift
@@ -3,13 +3,18 @@ import Foundation
 struct Actor: Codable {
     
     var actorName: String
-    var actorId: String
+    var actorId: Int
+    let apiKey: String
+    var apiURL: String
+    var url: URL
     
+    init() {
+        self.actorName = ""
+        self.actorId = 0
+        self.apiKey = "28acc3596024bbedbcf35d6718ffb64b"
+        self.apiURL = "https://api.themoviedb.org/3/search/person/?api_key=\(self.apiKey)&query="
+        self.url = URL(string: self.apiURL)!
+    }
 }
 
-let apiKey = "28acc3596024bbedbcf35d6718ffb64b"
-var url = URL(string: "https://api.themoviedb.org/3/search/person/?api_key=\(apiKey)&query=")
-
-URLSession.shared.dataTask(with: url)
-
-
+//move all of this to a UIViewController viewDidLoad func cause it seems like it's easier to do all of of this there 
